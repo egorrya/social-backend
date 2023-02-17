dad1309bde0d4a286beda58a4f94e85ab32a49b8
egorrya <fayncore@gmail.com>
2023-02-17 20:48:37 +0300

    refactored following and followers

--- controllers/Follow.controller.js
+++ controllers/Follow.controller.js
@@ -3,155 +3,149 @@ import FollowingModel from '../models/Following.model.js';
 import UserModel from '../models/User.model.js';
 
 export const toggleFollow = async (req, res) => {
-  try {
-    const followingUserId = req.body.id;
-    const userId = req.userId;
+	try {
+		const followingUserId = req.query.id;
+		const userId = req.userId;
 
-    const follow = await FollowingModel.findOne({
-      following_user_id: followingUserId,
-      user_id: userId,
-    });
+		const follow = await FollowingModel.findOne({
+			following_user_id: followingUserId,
+			user_id: userId,
+		});
 
-    if (followingUserId === userId) {
-      return res.status(400).json({
-        status: 'error',
-        message: "You can't follow yourself",
-      });
-    }
+		if (followingUserId === userId) {
+			return res.status(400).json({
+				status: 'error',
+				message: "You can't follow yourself",
+			});
+		}
 
-    if (!follow) {
-      await new FollowingModel({
-        following_user_id: followingUserId,
-        user_id: userId,
-      }).save();
+		if (!follow) {
+			await new FollowingModel({
+				following_user_id: followingUserId,
+				user_id: userId,
+			}).save();
 
-      await new FollowersModel({
-        follower_user_id: userId,
-        user_id: followingUserId,
-      }).save();
+			await new FollowersModel({
+				follower_user_id: userId,
+				user_id: followingUserId,
+			}).save();
 
-      await UserModel.updateOne(
-        { _id: userId },
-        { $push: { following: followingUserId } }
-      );
+			await UserModel.updateOne({ _id: userId }, { $inc: { following: 1 } });
 
-      await UserModel.updateOne(
-        { _id: followingUserId },
-        { $push: { followers: userId } }
-      );
+			await UserModel.updateOne(
+				{ _id: followingUserId },
+				{ $inc: { followers: 1 } }
+			);
 
-      return res.send({
-        status: 'success',
-        message: 'Follow successfully added',
-      });
-    } else {
-      await FollowersModel.findOneAndDelete({
-        follower_user_id: userId,
-        user_id: followingUserId,
-      });
+			return res.send({
+				status: 'success',
+				message: 'Follow successfully added',
+			});
+		} else {
+			await FollowersModel.findOneAndDelete({
+				follower_user_id: userId,
+				user_id: followingUserId,
+			});
 
-      await FollowingModel.findOneAndDelete({
-        following_user_id: followingUserId,
-        user_id: userId,
-      });
+			await FollowingModel.findOneAndDelete({
+				following_user_id: followingUserId,
+				user_id: userId,
+			});
 
-      await UserModel.updateOne(
-        { _id: userId },
-        { $pull: { following: followingUserId } }
-      );
+			await UserModel.updateOne({ _id: userId }, { $inc: { following: -1 } });
 
-      await UserModel.updateOne(
-        { _id: followingUserId },
-        { $pull: { followers: userId } }
-      );
+			await UserModel.updateOne(
+				{ _id: followingUserId },
+				{ $inc: { followers: -1 } }
+			);
 
-      return res.send({
-        status: 'success',
-        message: 'Follow successfully removed',
-      });
-    }
-  } catch (error) {
-    res.status(500).json({
-      status: 'error',
-      error,
-    });
-  }
+			return res.send({
+				status: 'success',
+				message: 'Follow successfully removed',
+			});
+		}
+	} catch (error) {
+		res.status(500).json({
+			status: 'error',
+			error,
+		});
+	}
 };
 
 export const getFollowersList = async (req, res) => {
-  try {
-    const userId = req.query.id || req.userId;
-    const limit = req.query.limit || 50;
-    const page = req.query.page || 1;
+	try {
+		const userId = req.query.id || req.userId;
+		const limit = req.query.limit || 50;
+		const page = req.query.page || 1;
 
-    const followers = await FollowersModel.find({ user_id: userId })
-      .populate({
-        path: 'follower_user_id',
-        match: {
-          active: true,
-        },
-        select: '-passwordHash',
-      })
-      .limit(limit)
-      .skip(limit * (page - 1))
-      .exec();
+		const followers = await FollowersModel.find({ user_id: userId })
+			.populate({
+				path: 'follower_user_id',
+				match: {
+					active: true,
+				},
+				select: '-passwordHash',
+			})
+			.limit(limit)
+			.skip(limit * (page - 1))
+			.exec();
 
-    const count = await FollowingModel.find({ user_id: userId }).count();
-    const lastPage = Math.ceil(count / limit);
+		const count = await FollowingModel.find({ user_id: userId }).count();
+		const lastPage = Math.ceil(count / limit);
 
-    res.json({
-      status: 'success',
+		res.json({
+			status: 'success',
 
-      count,
-      page,
-      limit,
-      last_page: lastPage,
-      data: followers,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      status: 'error',
-      message: 'Unable to get followers list',
-    });
-  }
+			count,
+			page,
+			limit,
+			last_page: lastPage,
+			data: followers,
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			status: 'error',
+			message: 'Unable to get followers list',
+		});
+	}
 };
 
 export const getFollowingList = async (req, res) => {
-  try {
-    const userId = req.query.id || req.userId;
-    const limit = req.query.limit || 50;
-    const page = req.query.page || 1;
+	try {
+		const userId = req.query.id || req.userId;
+		const limit = req.query.limit || 50;
+		const page = req.query.page || 1;
 
-    const following = await FollowingModel.find({ user_id: userId })
-      .populate({
-        path: 'following_user_id',
-        match: {
-          active: true,
-        },
-        select: '-passwordHash',
-      })
-      .limit(limit)
-      .skip(limit * (page - 1))
-      .exec();
+		const following = await FollowingModel.find({ user_id: userId })
+			.populate({
+				path: 'following_user_id',
+				match: {
+					active: true,
+				},
+				select: '-passwordHash',
+			})
+			.limit(limit)
+			.skip(limit * (page - 1))
+			.exec();
 
-    const count = await FollowingModel.find({ user_id: userId }).count();
-    const lastPage = Math.ceil(count / limit);
+		const count = await FollowingModel.find({ user_id: userId }).count();
+		const lastPage = Math.ceil(count / limit);
 
-    res.json({
-      status: 'success',
+		res.json({
+			status: 'success',
 
-      count,
-      page,
-      limit,
-      last_page: lastPage,
-      data: following,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      status: 'error',
-      message: 'Unable to get following list',
-    });
-  }
+			count,
+			page,
+			limit,
+			last_page: lastPage,
+			data: following,
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			status: 'error',
+			message: 'Unable to get following list',
+		});
+	}
 };
--- models/User.model.js
+++ models/User.model.js
@@ -1,33 +1,33 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema(
-  {
-    fullName: {
-      type: String,
-      required: false,
-    },
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    passwordHash: {
-      type: String,
-      required: true,
-    },
-    avatarUrl: String,
-    backgroundUrl: String,
-    following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Following' }],
-    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Followers' }],
-  },
-  {
-    timestamps: true,
-  }
+	{
+		fullName: {
+			type: String,
+			required: false,
+		},
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		passwordHash: {
+			type: String,
+			required: true,
+		},
+		avatarUrl: String,
+		backgroundUrl: String,
+		following: Number,
+		followers: Number,
+	},
+	{
+		timestamps: true,
+	}
 );
 
 export default mongoose.model('User', UserSchema);
